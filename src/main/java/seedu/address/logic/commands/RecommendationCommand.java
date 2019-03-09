a83d4944c02001ad01bc90abbac836d397215f3f
Philip Garabandic <philipgarabandic@gmail.com>
2019-03-09 21:40:09 +0800

    fixing travis

--- src/main/java/seedu/address/logic/commands/RecommendationCommand.java
+++ src/main/java/seedu/address/logic/commands/RecommendationCommand.java
@@ -6,8 +6,10 @@ import seedu.address.model.Model;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Return a recommended outfit to the user.
+ */
 public class RecommendationCommand extends Command {
-
 
     public static final String COMMAND_WORD = "recommendation";
 
@@ -16,7 +18,6 @@ public class RecommendationCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
-
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
