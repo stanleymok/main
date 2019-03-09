26ff616c8e8b23506a85995ddb2ce768328a2c85
Philip Garabandic <philipgarabandic@gmail.com>
2019-03-09 22:56:42 +0800

    CommandRecommendation added. Stores the last reccomended outfit and will execute the search.

---
+++ src/main/java/seedu/address/logic/CommandRecommendation.java
@@ -0,0 +1,13 @@
+package seedu.address.logic;
+
+import seedu.address.model.apparel.Apparel;
+
+/**
+ * Stores the recommendated outfit of CommandRecommendation.
+ */
+public class CommandRecommendation {
+    Apparel TOP, BOTTOM, BELT, SHOES;
+
+    public CommandRecommendation() {};
+
+}
