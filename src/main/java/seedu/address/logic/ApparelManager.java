7476f3118b2471ca6a3fa3be9fae0874ac1cd9eb
stanleymok <stanleyschoolwork@gmail.com>
2019-03-12 22:43:21 +0800

    add placeholder for apparelManager class to be implemented later

---
+++ src/main/java/seedu/address/logic/ApparelManager.java
@@ -0,0 +1,109 @@
+package seedu.address.logic;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+import javafx.beans.property.ReadOnlyProperty;
+import javafx.collections.ObservableList;
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.apparel.Apparel;
+import seedu.address.storage.Storage;
+
+/**
+ * The main LogicManager of the app.
+ */
+public class ApparelManager implements Logic {
+    public static final String FILE_OPS_ERROR_MESSAGE = "Could not save data to file: ";
+    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
+
+    private final Model model;
+    private final Storage storage;
+    private final CommandHistory history;
+    private final AddressBookParser addressBookParser;
+    private boolean addressBookModified;
+
+    public ApparelManager(Model model, Storage storage) {
+        this.model = model;
+        this.storage = storage;
+        history = new CommandHistory();
+        addressBookParser = new AddressBookParser();
+
+        // Set addressBookModified to true whenever the models' address book is modified.
+        model.getAddressBook().addListener(observable -> addressBookModified = true);
+    }
+
+    @Override
+    public CommandResult execute(String commandText) throws CommandException, ParseException {
+        logger.info("----------------[USER COMMAND][" + commandText + "]");
+        addressBookModified = false;
+
+        CommandResult commandResult;
+        try {
+            Command command = addressBookParser.parseCommand(commandText);
+            commandResult = command.execute(model, history);
+        } finally {
+            history.add(commandText);
+        }
+
+        if (addressBookModified) {
+            logger.info("Address book modified, saving to file.");
+            try {
+                storage.saveAddressBook(model.getAddressBook());
+            } catch (IOException ioe) {
+                throw new CommandException(FILE_OPS_ERROR_MESSAGE + ioe, ioe);
+            }
+        }
+
+        return commandResult;
+    }
+
+    @Override
+    public ReadOnlyAddressBook getAddressBook() {
+        return model.getAddressBook();
+    }
+
+    @Override
+    public ObservableList<Apparel> getFilteredPersonList() {
+        return model.getFilteredApparelList();
+    }
+
+    @Override
+    public ObservableList<String> getHistory() {
+        return history.getHistory();
+    }
+
+    @Override
+    public Path getAddressBookFilePath() {
+        return model.getAddressBookFilePath();
+    }
+
+    @Override
+    public GuiSettings getGuiSettings() {
+        return model.getGuiSettings();
+    }
+
+    @Override
+    public void setGuiSettings(GuiSettings guiSettings) {
+        model.setGuiSettings(guiSettings);
+    }
+
+    @Override
+    public ReadOnlyProperty<Apparel> selectedPersonProperty() {
+        return model.selectedPersonProperty();
+    }
+
+    @Override
+    public void setSelectedPerson(Apparel apparel) {
+        model.setSelectedPerson(apparel);
+    }
+}
+
