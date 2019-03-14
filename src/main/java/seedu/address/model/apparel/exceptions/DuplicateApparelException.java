df93389e92da32fd2be0e9de794686aa040ff309
Philip Garabandic <philipgarabandic@gmail.com>
2019-03-14 21:45:18 +0800

    work

--- src/main/java/seedu/address/model/apparel/exceptions/DuplicateApparelException.java
+++ src/main/java/seedu/address/model/apparel/exceptions/DuplicateApparelException.java
@@ -1,7 +1,8 @@
 package seedu.address.model.apparel.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Apparels (Apparels are considered duplicates if they have the same
+ * Signals that the operation will result in duplicate Apparels
+ * (Apparels are considered duplicates if they have the same
  * identity).
  */
 public class DuplicateApparelException extends RuntimeException {
