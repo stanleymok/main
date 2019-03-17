807c634ac0f74bf2e27d749a4bec008601b1fb05
Philip Garabandic <philipgarabandic@gmail.com>
2019-03-17 15:23:33 +0800

    added more functionality to CommandRecommendation

--- src/main/java/seedu/address/logic/CommandRecommendation.java
+++ src/main/java/seedu/address/logic/CommandRecommendation.java
@@ -1,47 +1,78 @@
 package seedu.address.logic;
 
+import java.util.ArrayList;
+
+import javafx.collections.ObservableList;
+import seedu.address.model.Model;
 import seedu.address.model.apparel.Apparel;
+import seedu.address.model.apparel.ClothingType;
+import seedu.address.model.apparel.Color;
 
 /**
  * Stores the recommendated outfit of CommandRecommendation.
  */
 public class CommandRecommendation {
-    private Apparel top;
-    private Apparel bottom;
-    private Apparel belt;
-    private Apparel shoes;
+    private ArrayList<Apparel> tops = new ArrayList<>();
+    private ArrayList<Apparel> belts = new ArrayList<>();
+    private ArrayList<Apparel> shoes = new ArrayList<>();
+    private ArrayList<Apparel> bottoms = new ArrayList<>();
+    private Apparel reccomendedTop;
+    private Apparel reccomendedBelt;
+    private Apparel reccomendedShoe;
+    private Apparel reccomendedBottom;
 
-    public CommandRecommendation() {};
 
-    public Apparel getTop() {
-        return top;
+    public CommandRecommendation(Model model) {
+        setClothingInSections(model.getFilteredApparelList());
+        reccomendedBottom = reccomendBottom();
+        reccomendedTop = reccomendTop(reccomendedBottom.getColor());
+        reccomendedBelt = reccomendBelt();
+        reccomendedShoe = reccomendShoe();
+
     }
 
-    public void setTop(Apparel top) {
-        this.top = top;
+    private Apparel reccomendShoe() {
+        if(shoes.size() > 0){
+            return null;
+        }
+
+        return shoes.get(0);
     }
 
-    public Apparel getBottom() {
-        return bottom;
+    private Apparel reccomendBelt() {
+        if(belts.size() > 0){
+            return null;
+        }
+        return belts.get(0);
     }
 
-    public void setBottom(Apparel bottom) {
-        this.bottom = bottom;
+    private Apparel reccomendTop(Color bottomColor) {
+        if(tops.size() > 0){
+            return null;
+        }
+        return bottoms.get(0);
     }
 
-    public Apparel getBelt() {
-        return belt;
+    private Apparel reccomendBottom() {
+        if(bottoms.size() > 0){
+            return null;
+        }
+        int index = (int)(Math.random() * bottoms.size());
+        return bottoms.get(index);
     }
 
-    public void setBelt(Apparel belt) {
-        this.belt = belt;
+    private void setClothingInSections(ObservableList<Apparel> filteredApparelList) {
+        for (int i = 0; i < filteredApparelList.size(); i++){
+            Apparel apperal = filteredApparelList.get(i);
+            ClothingType type = apperal.getClothingType();
+            switch (type.getClothingTypeValue()) {
+                case TOP: tops.add(apperal);
+                case BELT: belts.add(apperal);
+                case SHOES: shoes.add(apperal);
+                case BOTTOM: bottoms.add(apperal);
+            }
+        }
+
     }
 
-    public Apparel getShoes() {
-        return shoes;
-    }
-
-    public void setShoes(Apparel shoes) {
-        this.shoes = shoes;
-    }
 }
