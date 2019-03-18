5af629ca99b35920719e055ade6170b3c9fba583
ReinSheer <eugene544@hotmail.com>
2019-03-18 16:53:02 +0800

    Fix checkstyle #3

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -21,10 +21,6 @@ import seedu.address.model.apparel.Apparel;
 import seedu.address.model.apparel.NameContainsKeywordsPredicate;
 import seedu.address.testutil.EditApparelDescriptorBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Contains helper methods for testing commands.
  */
@@ -37,9 +33,6 @@ public class CommandTestUtil {
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_TAG_HUSBAND = "husband";
-    public static final String VALID_TAG_FRIEND = "friend";
 
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
