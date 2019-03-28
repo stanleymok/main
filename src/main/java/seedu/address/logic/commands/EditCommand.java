4b8996092e4a49df897dba14467deaa0a23f0392
stanleymok <stanleyschoolwork@gmail.com>
2019-03-28 14:36:30 +0800

    Edit help message of for edit command

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -34,6 +34,7 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the apparel identified "
             + "by the index number used in the displayed apparel list. "
+            + "To edit availability use dirty/unavailable and wash/available commands. "
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
