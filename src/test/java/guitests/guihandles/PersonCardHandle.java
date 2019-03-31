22dc6b4bea86b194bb5d2a8cc416f5be5021b6ec
stanleymok <stanleyschoolwork@gmail.com>
2019-04-01 03:53:04 +0800

    Fix checkstyle

--- src/test/java/guitests/guihandles/PersonCardHandle.java
+++ src/test/java/guitests/guihandles/PersonCardHandle.java
@@ -15,8 +15,8 @@ public class PersonCardHandle extends NodeHandle<Node> {
     private static final String ID_FIELD_ID = "#id";
     private static final String NAME_FIELD_ID = "#name";
     //private static final String ADDRESS_FIELD_ID = "#address";
-   // private static final String PHONE_FIELD_ID = "#phone";
-   // private static final String EMAIL_FIELD_ID = "#email";
+    //private static final String PHONE_FIELD_ID = "#phone";
+    //private static final String EMAIL_FIELD_ID = "#email";
     private static final String TAGS_FIELD_ID = "#tags";
 
     private final Label idLabel;
@@ -31,9 +31,9 @@ public class PersonCardHandle extends NodeHandle<Node> {
 
         idLabel = getChildNode(ID_FIELD_ID);
         nameLabel = getChildNode(NAME_FIELD_ID);
-      //  addressLabel = getChildNode(ADDRESS_FIELD_ID);
-      //  phoneLabel = getChildNode(PHONE_FIELD_ID);
-      //  emailLabel = getChildNode(EMAIL_FIELD_ID);
+        //addressLabel = getChildNode(ADDRESS_FIELD_ID);
+        //phoneLabel = getChildNode(PHONE_FIELD_ID);
+        //emailLabel = getChildNode(EMAIL_FIELD_ID);
 
         Region tagsContainer = getChildNode(TAGS_FIELD_ID);
         tagLabels = tagsContainer
@@ -75,7 +75,7 @@ public class PersonCardHandle extends NodeHandle<Node> {
      */
     public boolean equals(Apparel apparel) {
         return getName().equals(apparel.getName().fullName);
-     //           && getPhone().equals(apparel.getColor())
-     //           && getEmail().equals(apparel.getClothingType());
+        //     && getPhone().equals(apparel.getColor())
+        //     && getEmail().equals(apparel.getClothingType());
     }
 }
