753398f0fbe912d072584e8013eb0c5b0a08b8ea
stanleymok <stanleyschoolwork@gmail.com>
2019-04-09 15:06:48 +0800

    Clean up redundant global var

--- src/main/java/seedu/address/logic/commands/AvailableCommand.java
+++ src/main/java/seedu/address/logic/commands/AvailableCommand.java
@@ -36,7 +36,6 @@ public class AvailableCommand extends Command {
 
     public static final String MESSAGE_CLEAN_APPAREL_SUCCESS = "Apparel %1$d. washed: \n%2$s";
     public static final String MESSAGE_APPAREL_ALREADY_CLEAN_OCD = "Apparel %1$d. already washed: \n%2$s";
-    public static final String MESSAGE_NOT_EDITED = "Apparel index must be provided.";
     public static final String MESSAGE_DUPLICATE_APPAREL = "This apparel already exists in the address book.";
 
     private final Index index;
