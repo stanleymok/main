d1821a3dcbf42df327c8bf15ed75322ce0fa7312
rafflesandrison <rafflesandrison@u.nus.edu>
2019-04-12 03:22:08 +0800

    Update Browser Planel UI

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -1,67 +1,82 @@
 package seedu.address.ui;
 
-import static java.util.Objects.requireNonNull;
-
-import java.net.URL;
+import java.io.File;
 import java.util.logging.Logger;
 
-import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
-import javafx.scene.web.WebView;
-import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.apparel.Apparel;
+
 
 /**
  * The Browser Panel of the App.
  */
 public class BrowserPanel extends UiPart<Region> {
 
-    public static final URL DEFAULT_PAGE =
-            requireNonNull(MainApp.class.getResource(FXML_FILE_FOLDER + "default.html"));
-    public static final String SEARCH_PAGE_URL = "https://se-education.org/dummy-search-page/?name=";
-
     private static final String FXML = "BrowserPanel.fxml";
 
     private final Logger logger = LogsCenter.getLogger(getClass());
 
     @FXML
-    private WebView browser;
+    private ImageView apparelImageView;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label clothingTypeLabel;
+    @FXML
+    private Label colorLabel;
+    @FXML
+    private Label usageCountLabel;
+    @FXML
+    private Label statusLabel;
 
-    public BrowserPanel(ObservableValue<Apparel> selectedPerson) {
+    public BrowserPanel(ObservableValue<Apparel> selectedApparel) {
         super(FXML);
 
         // To prevent triggering events for typing inside the loaded Web page.
         getRoot().setOnKeyPressed(Event::consume);
 
-        // Load apparel page when selected apparel changes.
-        selectedPerson.addListener((observable, oldValue, newValue) -> {
+        selectedApparel.addListener((observable, oldValue, newValue) -> {
             if (newValue == null) {
-                loadDefaultPage();
-                return;
+                showApparelDetails(null);
+            } else {
+                showApparelDetails(newValue);
             }
-            loadPersonPage(newValue);
         });
-
-        loadDefaultPage();
     }
 
-    private void loadPersonPage(Apparel apparel) {
-        loadPage(SEARCH_PAGE_URL + apparel.getName().fullName);
-    }
-
-    public void loadPage(String url) {
-        Platform.runLater(() -> browser.getEngine().load(url));
-    }
 
     /**
-     * Loads a default HTML file with a background that matches the general theme.
+     * Fill all text fields to show details about the apparel.
+     * If the specified apparel is null, all text fields are cleared.
      */
-    private void loadDefaultPage() {
-        loadPage(DEFAULT_PAGE.toExternalForm());
+    private void showApparelDetails(Apparel apparel) {
+        // TODO
+        File file = new File("/images/address_book_32.png");
+        Image image = new Image(file.toURI().toString());
+        apparelImageView.setImage(image);
+        if (apparel == null) {
+            nameLabel.setText("");
+            clothingTypeLabel.setText("");
+            colorLabel.setText("");
+            usageCountLabel.setText("");
+            statusLabel.setText("");
+        } else {
+            // Fill the labels with info from the apparel Object
+            System.out.println("apparel = " + apparel.toString());
+            System.out.println("apparel usage = " + apparel.getUsageCount());
+            nameLabel.setText(apparel.getName().fullName);
+            clothingTypeLabel.setText(apparel.getClothingType().toString());
+            colorLabel.setText(apparel.getColor().toString());
+            usageCountLabel.setText(String.valueOf(apparel.getUsageCount()));
+            statusLabel.setText(apparel.getAvailabilityStatus());
+        }
     }
 
 }
